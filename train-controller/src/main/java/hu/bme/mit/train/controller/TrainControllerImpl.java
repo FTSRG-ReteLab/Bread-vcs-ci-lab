5a2c51b7371835a56b31300cce9b767020f42400
meres <meres@ubuntu>
2018-02-26 06:29:35 -0800

    step renamed to stepperValue

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -4,7 +4,7 @@ import hu.bme.mit.train.interfaces.TrainController;
 
 public class TrainControllerImpl implements TrainController {
 
-	private int step = 0;
+	private int stepperValue = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 10;
 
@@ -13,8 +13,8 @@ public class TrainControllerImpl implements TrainController {
 		if (referenceSpeed < 0) {
 			referenceSpeed = 0;
 		} else {
-		    if(referenceSpeed+step > 0) {
-                referenceSpeed += step;
+		    if(referenceSpeed+ stepperValue > 0) {
+                referenceSpeed += stepperValue;
             } else {
 		        referenceSpeed = 0;
             }
@@ -43,7 +43,7 @@ public class TrainControllerImpl implements TrainController {
 
 	@Override
 	public void setJoystickPosition(int joystickPosition) {
-		this.step = joystickPosition;		
+		this.stepperValue = joystickPosition;
 	}
 
 }
